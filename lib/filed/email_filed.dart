d4fb3b52dbc86d1ea54dd459efdae788a2c8d3e6
shazad02 <rayhanrahul9f@gmail.com>
2023-01-24 17:16:20 +0700

    update

--- lib/filed/email_filed.dart
+++ lib/filed/email_filed.dart
@@ -11,23 +11,25 @@ class EmailWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.height * 10 / 10,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: Theme.of(context).disabledColor,
       ),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const Text(
               'Email: ',
+              overflow: TextOverflow.ellipsis,
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             Text(
               emailName,
-              maxLines: 100,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
           ],
         ),
